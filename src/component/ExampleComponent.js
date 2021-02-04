93a0ad549390a31f78e1a42d0bd7f00c400156c4
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-02-04 15:37:34 +0000

    Testing main branch pull request execution

--- src/component/ExampleComponent.js
+++ src/component/ExampleComponent.js
@@ -9,6 +9,7 @@ export default function ExampleComponent({ propertyA, propertyB }) {
       <h4>{propertyA}</h4>
       <h3>I'm propertyB</h3>
       <h4>{propertyB}</h4>
+      <h3>Will i trigger a pipeline execution on main branch?Stay tuned!</h3>
     </>
   );
 }
